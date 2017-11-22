9c2af1b4ca516c3ed1cda036d30289bb9d4f5941
Israel Hofman <icehofman@hotmail.com>
2017-11-22 14:32:17 -0200

    Fix

--- devops-maturity-app/karma.conf.js
+++ devops-maturity-app/karma.conf.js
@@ -30,4 +30,21 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false
   });
+
+  var configuration = {
+    // other things
+  
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+  };
+  
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+  
+  config.set(configuration);
 };
