029f4d1673585d8a3e139ef61d2b90b3aadb9680
Israel Hofman <icehofman@hotmail.com>
2017-11-23 13:51:53 -0200

    Add route question

--- devops-maturity-app/src/app/app.module.ts
+++ devops-maturity-app/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
 import { AppComponent } from './app.component';
-
+import { QuestionsComponent } from './questions/questions.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
