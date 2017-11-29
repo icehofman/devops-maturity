ff79dcd53a316ef8c6683bd15ad3bd3b6f90ee57
Israel Hofman <icehofman@hotmail.com>
2017-11-29 21:31:05 -0200

    Validate HttpClient

--- devops-maturity-app/src/app/app.component.ts
+++ devops-maturity-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Iquestions } from './iquestions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devops-maturity-app';
+
+  results = '';
+  
+    constructor(private http: HttpClient){ }
+  
+    ngOnInit(): void { 
+      this.http.get<Iquestions>('http://localhost:3000/questions/1').subscribe(
+        data => {
+          console.log("id: " + data.id);
+          console.log("Questão: " + data.questao);
+          console.log("Resposta: " + data.diretriz);
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
+        }
+      );
+    }
 }
