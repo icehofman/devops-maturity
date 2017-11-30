8c0d21efff83379147006653e9fd70500c977e94
Israel Hofman <icehofman@hotmail.com>
2017-11-30 10:13:40 -0200

    add test

--- devops-maturity-app/src/app/app.module.ts
+++ devops-maturity-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { QuestionsService } from './questions.service';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [QuestionsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- devops-maturity-app/src/app/questions.service.ts
+++ devops-maturity-app/src/app/questions.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class QuestionsService {
-
-  constructor() { }
-
+  baseUrl:string = "http://localhost:3000";
+  
+  constructor(private http : HttpClient) { }
+  
+  get_questions(){
+    return this.http.get(this.baseUrl + '/questions');
+  }
 }
